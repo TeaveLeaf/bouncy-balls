3fd93cdf8d820329fbb6431009f653f15b57434b
TeaveLeaf <116187490+TeaveLeaf@users.noreply.github.com>
2023-05-18 10:16:56 -0700

    Made balls

--- main.js
+++ main.js
@@ -12,3 +12,85 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function() {
+  ctx.beginPath(); //Draws the shape on the paper
+  ctx.fillStyle = this.color; //Defines the color the shape will be
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI); //Traces arc shape on paper
+  ctx.fill(); //Basically finishes drawing the path and fills up all the space it takes up
+}
+
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX)
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+let balls = [];
+
+while (balls.length < 50) {
+  let size = random(10,20);
+  let ball = new Ball(
+    random(0 + size,width - size),
+    random(0 + size,height - size),
+    random(-7,7),
+    random(-7,7),
+    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
+      size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for(let i = 0; i < balls.length; i++){
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+Ball.prototype.collisionDetect = function() {
+  for(let j = 0; j < balls.length; j++) {
+    if(!(this === balls[j])) {
+      const dx = this.x - balls[j].x;
+      const dy = this.y - balls[j].y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if(distance < this.size + balls[j].size) {
+        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0, 255) + ',' + random(0,255) + ')';
+      }
+    }
+  }
+}
+
+loop();
